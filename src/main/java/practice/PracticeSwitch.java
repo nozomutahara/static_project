3cd314a27138ace1eb569e8d3ccdad10cbfe3358
LENOVO-THINKPAD\user <nanano0318@gmail.com>
2023-05-25 18:24:03 +0900

    first commit.

---
+++ src/main/java/practice/PracticeSwitch.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeSwitch {
+
+	public static void main(String[] args) {
+		int array[] = {1,2,3,4,5};
+		String job[] = {"勇者","バトルマスター","パラディン","海賊","魔法戦士"};
+
+		for(int i = 0; i < 5; i++) {
+			System.out.println(array[i] + ":" + job[i]);
+		}
+	}
+
+}
