834849cfdc3c265919fdbbb9988d3c6cae16ad70
LENOVO-THINKPAD\user <nanano0318@gmail.com>
2023-05-25 17:38:35 +0900

    first commit;

---
+++ src/main/java/practice/PracticeFor.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeFor {
+
+	public static void main(String[] args) {
+		int num = 0;
+		
+		for(int i = 0; i <= 100; i++) {
+			num += i;
+		}
+		System.out.println(num);
+	}
+
+}
