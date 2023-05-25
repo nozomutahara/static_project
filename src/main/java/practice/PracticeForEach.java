979bc557ba15f33a12eb161a57a5f28d32bc0838
LENOVO-THINKPAD\user <nanano0318@gmail.com>
2023-05-25 17:53:13 +0900

    first commit.

---
+++ src/main/java/practice/PracticeForEach.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class PracticeForEach {
+
+	public static void main(String[] args) {
+		String lists[] = {"三上ゆあ","明日花きらら","蒼井そら","Rio","麻美ゆま","岸あいの","数井哲","広瀬ゆり","朝日りお","紗倉まな"};
+		
+		for(String list: lists) {
+			System.out.println(list);
+		}
+
+	}
+
+}
