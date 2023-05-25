c18c8ae8c1cfb4c0f8b123951d7fbe6b9e11aad4
LENOVO-THINKPAD\user <nanano0318@gmail.com>
2023-05-25 16:35:15 +0900

    first commit.

--- src/main/java/static_project/Sample.java
+++ src/main/java/static_project/Sample.java
@@ -3,8 +3,28 @@ package static_project;
 public class Sample {
 
 	public static void main(String[] args) {
-		System.out.println("Sampleです");
-		System.out.println("Java");
+		
+		int value = 28;
+
+		
+		if (value > 27) {
+			System.out.println("予想最高気温は、" + value + "です。熱中症に注意して下さい。");
+		}
+		else {
+			System.out.println("予想最高気温は、" +value + "です。熱中症に注意して下さい。");
+		}
+		
+		
+		int value1 = 8;
+		int answer;
+		
+		answer = value1 / 3;
+		
+		
+		
+		
+		
+		
 	}
 
 }
